ce2485126b872acf6682240b40f036587f7fa08e
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-26 11:22:05 -0500

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
+import { Counter, User } from './components';
 
 function App(): JSX.Element {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Vite + React + TypeScript</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <hr />
+      {/* UseState */}
+      <Counter />
+      <User />
     </>
   );
 }
