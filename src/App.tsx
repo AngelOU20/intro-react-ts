e5ae57b1f89ebcf2a09e445a89b18eb2e79beb98
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-26 12:47:58 -0500

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { Counter, User } from './components';
+import { TimerFather } from './components/TimerFather';
 
 function App(): JSX.Element {
   return (
     <>
-      <h1>Vite + React + TypeScript</h1>
+      <h1>React + Hooks + TypeScript</h1>
       <hr />
       {/* UseState */}
-      <Counter />
-      <User />
+      <div className="container-hook">
+        <h2 className="title-hook" style={{ color: '#646cff' }}>
+          useState
+        </h2>
+        <Counter />
+        <User />
+      </div>
+
+      {/* useEffect - useRef */}
+      <div className="container-hook">
+        <h2 className="title-hook" style={{ color: '#77ffff' }}>
+          useEffect - useRef
+        </h2>
+        <TimerFather />
+      </div>
     </>
   );
 }
