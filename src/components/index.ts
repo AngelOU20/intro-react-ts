4f722759a2f88673622174de3abf162f24d40a82
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-27 12:53:48 -0500

    updated barrel file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export * from './Counter';
 export * from './CounterRed';
+export * from './Form';
+export * from './Form2';
 export * from './Timer';
 export * from './TimerFather';
 export * from './User';
