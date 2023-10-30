0ab0d2af33e37b373f1b9ff564400185826e2c16
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-30 10:43:26 -0500

    updated Navigation

--- 03-react-app/src/router/Navigation.tsx
+++ 03-react-app/src/router/Navigation.tsx
@@ -1,49 +1,39 @@
-import { NavLink, Navigate, Route, Routes } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/react.svg';
 
 export const Navigation = () => {
   return (
-    <div className="main-layout">
-      <nav>
-        <a href="#">
-          <img src={logo} className="logo react" alt="React logo" />
-        </a>
+    <nav>
+      <a href="#">
+        <img src={logo} className="logo react" alt="React logo" />
+      </a>
 
-        <ul>
-          <li>
-            <NavLink
-              to="home"
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="about"
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-            >
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="users"
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-            >
-              User
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="home" element={<h1>Home Page</h1>} />
-        <Route path="about" element={<h1>About Page</h1>} />
-        <Route path="users" element={<h1>Users Page</h1>} />
-
-        <Route path="*" element={<Navigate to="/home" replace />} />
-      </Routes>
-    </div>
+      <ul>
+        <li>
+          <NavLink
+            to="lazy1"
+            className={({ isActive }) => (isActive ? 'nav-active' : '')}
+          >
+            Lazy 1
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="lazy2"
+            className={({ isActive }) => (isActive ? 'nav-active' : '')}
+          >
+            Lazy 2
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="lazy3"
+            className={({ isActive }) => (isActive ? 'nav-active' : '')}
+          >
+            Lazy 3
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
