59ea264af46c78c50fee56fd36c161a57e316779
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-06 17:45:06 -0500

    delete folders

--- 00-CleanSolid/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import './solid-principles/05-dependency-a';
-
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.innerHTML = `
-  <h1>CleanCode y SOLID</h1>
-  <span>Revisar la consola de JavaScript</span>
-`;
--- 02-hooks-with-ts/src/app/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Counter';
-export * from './CounterRed';
-export * from './Form';
-export * from './Form2';
-export * from './Timer';
-export * from './TimerFather';
-export * from './User';
