6ecb21f32d35cb76c6de39313d547dd3128fc7df
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-06 17:14:19 -0500

    delete 00-CleanSolid

--- 00-CleanSolid/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import './code-smells/02-low-coupling';
-
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.innerHTML = `
-  <h1>CleanCode y SOLID</h1>
-  <span>Revisar la consola de JavaScript</span>
-`;
